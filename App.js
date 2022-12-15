481f2193f982d24936fa360a57155db53286870e
Wei-Meng Lee <weimenglee@learn2develop.net>
2022-12-15 10:36:50 +0800

    React web3 front-end

--- App.js
+++ App.js
@@ -29,7 +29,7 @@ class App extends React.Component {
       document : "" 
     };
   }
-
+ 
   //-------------------------------------
   //* Get the current account in ZilPay *
   //-------------------------------------
